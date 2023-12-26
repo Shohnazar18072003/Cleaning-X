2328e048bdaf58394aa23f2500a94c92329ab053
Shohnazar2003 <129948756+Shohnazar2003@users.noreply.github.com>
2023-12-26 07:41:49 +0500

    Cleaning X project done !

---
+++ README.md
@@ -0,0 +1,3 @@
+CREATING X  ! ! ! ! ! ! ! ! ! 
+
+Figma link : https://www.figma.com/file/SKewzRNZW9VcUyS7oIbWtS/Cleaning-X?node-id=1%3A1595&mode=dev
---
+++ src/components/Main/index.js
@@ -0,0 +1,7 @@
+import About_us from "./About_us/About_us";
+import Contacts from "./Contacts/Contacts";
+import Hero from "../Main/Hero/Hero";
+import Services from "./Services/Services";
+import Articles from "./Articles/Articles";
+
+export {About_us, Contacts, Hero, Services, Articles}
